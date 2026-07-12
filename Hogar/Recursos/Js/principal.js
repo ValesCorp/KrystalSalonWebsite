6100ce340c1213ba18ffdb07fbde1b6ef8f9aaf8
David Vales <davidvales@MacBook-Pro-de-David.local>
2026-07-12 17:29:55 -0600

    subida inicial y proyecto inicial

---
+++ Hogar/Recursos/Js/principal.js
@@ -0,0 +1,16 @@
+// principal.js JavaScript principal del sitio web
+// Vales Corp
+//
+$(document).ready(function() {
+    $(".ui.toggle.button").click(function() {
+        $(".mobile.only.grid .ui.vertical.menu").toggle(100);
+    });
+
+    $(".ui.dropdown").dropdown();
+
+    $(".slide").slick({
+        autoplay: true,
+        dots: true,
+        speed: 500
+    });
+});
